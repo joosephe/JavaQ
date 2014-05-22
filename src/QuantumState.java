4c7b6a42824dbcf0658efb43f88e44d80af636b8
Toomas Krips <toomaskrips@gmail.com>
2014-05-22 18:03:48 +0300

    Lisasin QuantumState'i klassile kvantolekule qubiti lisamise

--- src/QuantumState.java
+++ src/QuantumState.java
@@ -4,12 +4,26 @@ public class QuantumState {
 	
 	public Complex[] stateVector;
 	
-	//blah
+	//Quantumstate from a qubit
 	public QuantumState(Qubit a){
 		Complex[] v = new Complex[2];
 		v[0]= a.noPart;
 		v[1]=a.yesPart;
 		this.stateVector=v;
+	}
+	
+	//This is sort of like the tensor product part.
+	//We add a new qubit to the Quantum State.
+	//Maybe this should be some other type of a method?
+	public  QuantumState (QuantumState qs, Qubit qb){
+		int len = qs.stateVector.length;
+		Complex[] v = new Complex[2*len];
+		for(int i=0;i<len;i++){
+			v[i]=Complex.Multiply(qs.stateVector[i],qb.noPart);
+			v[i+len]=Complex.Multiply(qs.stateVector[i], qb.yesPart);		
+		}
+		
+		this. stateVector =v;
 	}
 	
 
