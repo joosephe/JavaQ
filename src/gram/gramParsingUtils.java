47780f4b4ac9e81e59af66c953467072a6debded
Your Name <kaspar93@msn.com>
2014-05-25 22:28:34 +0300

    utils for ast

---
+++ src/gram/gramParsingUtils.java
@@ -0,0 +1,27 @@
+package gram;
+
+import org.antlr.v4.runtime.ANTLRInputStream;
+import org.antlr.v4.runtime.CommonTokenStream;
+import org.antlr.v4.runtime.tree.ParseTree;
+
+import ee.ut.cs.akt.aktk.ast.AstNode;
+
+public class gramParsingUtils {
+	static public AstNode createAst(String program) {
+		ParseTree tree = createParseTree(program);
+		gramVisitor<AstNode> visitor = new gramAstCreationVisitor();
+		return tree.accept(visitor);
+	}
+	
+	
+	private static ParseTree createParseTree(String program) {
+		ANTLRInputStream antlrInput = new ANTLRInputStream(program);
+		gramLexer lexer = new gramLexer(antlrInput);
+        CommonTokenStream tokens = new CommonTokenStream(lexer);
+        gramParser parser = new gramParser(tokens);
+        ParseTree tree = parser.program();
+        //System.out.println(tree.toStringTree(parser));
+        return tree;
+	}
+	
+}
